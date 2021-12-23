e5c20a9e9c1067aeb925a09a0c538282968ad81e
infinite-idea <infinite.idea9810@outlook.com>
2021-12-23 18:47:32 -0500

    seconde update

--- authServer.js
+++ authServer.js
@@ -1,15 +1,25 @@
 require("dotenv").config()
-//this will allow us to pull params from .env file
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
 const users = []
 const jwt = require("jsonwebtoken")
+const port = process.env.TOKEN_SERVER_PORT
+let refreshTokens = []
+function generateAccessToken(user) {
+    return
+    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" })
+}
+
+function generateRefreshToken(user) {
+    const refreshToken =
+        jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "20m" })
+    refreshTokens.push(refreshToken)
+    return refreshToken
+}
 
 app.use(express.json())
-//This middleware will allow us to pull req.body.<params>
-const port = process.env.TOKEN_SERVER_PORT
-//get the port number from .env file
+
 app.listen(port, () => {
     console.log(`Authorization Server running on ${port}...`)
 })
@@ -21,12 +31,10 @@ app.post("/createUser", async (req, res) => {
     console.log(users)
 })
 
-//AUTHENTICATE LOGIN AND RETURN JWT TOKEN
+
 app.post("/login", async (req, res) => {
     const user = users.find((c) => c.user == req.body.name)
-    //check to see if the user exists in the list of registered users
     if (user == null) res.status(404).send("User does not exist!")
-    //if user does not exist, send a 400 response
     if (await bcrypt.compare(req.body.password, user.password)) {
         const accessToken = generateAccessToken({ user: req.body.name })
         const refreshToken = generateRefreshToken({ user: req.body.name })
@@ -36,3 +44,17 @@ app.post("/login", async (req, res) => {
         res.status(401).send("Password Incorrect!")
     }
 })
+
+app.post("/refreshToken", (req, res) => {
+    if (!refreshTokens.includes(req.body.token)) res.status(400).send("Refresh Token Invalid")
+    refreshTokens = refreshTokens.filter((c) => c != req.body.token)
+    const accessToken = generateAccessToken({ user: req.body.name })
+    const refreshToken = generateRefreshToken({ user: req.body.name })
+    res.json({ accessToken: accessToken, refreshToken: refreshToken })
+})
+
+app.delete("/logout", (req, res) => {
+    refreshTokens = refreshTokens.filter((c) => c != req.body.token)
+    res.status(204).send("Logged out!")
+})
+
--- validateToken.js
+++ validateToken.js
@@ -4,7 +4,6 @@ const app = express()
 app.use(express.json())
 const jwt = require("jsonwebtoken")
 const port = process.env.PORT
-//We will run this server on a different port i.e. port 5000
 app.listen(port, () => {
     console.log(`Validation server running on ${port}...`)
 })
@@ -15,10 +14,8 @@ app.get("/posts", validateToken, (req, res) => {
 })
 
 function validateToken(req, res, next) {
-    //get token from request header
     const authHeader = req.headers["authorization"]
     const token = authHeader.split(" ")[1]
-    //the request header contains the token "Bearer <token>", split the string and use the second value in the split array.
     if (token == null) res.sendStatus(400).send("Token not present")
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) {
@@ -26,7 +23,7 @@ function validateToken(req, res, next) {
         }
         else {
             req.user = user
-            next() //proceed to the next action in the calling function
+            next() 
         }
-    }) //end of jwt.verify()
-} //end of function
+    }) 
+}
